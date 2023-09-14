97f6f8a042d7716c4485c4da9a47199c60f3fb02
Aarya <103888895+chhateauuu@users.noreply.github.com>
2023-09-14 19:49:37 -0400

    Update DatabaseConnection.java

--- atm simulator/src/main/java/DatabaseConnection.java
+++ atm simulator/src/main/java/DatabaseConnection.java
@@ -6,7 +6,7 @@ public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/ATM_Simulator";
     private static final String USER = "aaryabratc";
-    private static final String PASSWORD = "NAIMABHANDINA0520";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
